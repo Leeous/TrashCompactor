8107492593b2be5df86447bc1a95061740100a42
Cody Lee Fields <contact@leeous.com>
2026-05-07 15:39:50 -0400

    feat: add ImpactSpeedNeededToDamage property to PhysicsDamageHandler

--- Code/Utility/PhysicsDamageHandler.cs
+++ Code/Utility/PhysicsDamageHandler.cs
@@ -4,17 +4,24 @@ namespace Sandbox;
 
 public sealed class PhysicsDamageHandler : Component, Component.ICollisionListener
 {
+	/// <summary>
+	/// Enables logging for this component.
+	/// </summary>
 	[Property] public bool EnableLogging { get; set; } = false;
+	/// <summary>
+	/// The impact speed needed to register damage to a player.
+	/// 
+	/// Warning: Values that are too low can cause players to take damage by simply jumping into a wall.
+	/// </summary>
+	[Property, Title("Impact damage speed"), Description("The minimum impact speed needed to damage a player.")] public float ImpactSpeedNeededToDamage { get; set; } = 500f;
 	
 	public void OnCollisionStart( Collision collision )
 	{
 		if (!collision.Other.Body.GameObject.Tags.Contains( "prop" )) return;
 		
 		float impact = Math.Abs(collision.Self.Body.Velocity.x + collision.Self.Body.Velocity.y);
-		
-		Log.Info( $"Player took ${impact} damage." );
-		
-		if ( impact > 500  )
+				
+		if ( impact > ImpactSpeedNeededToDamage  )
 		{
 			if ( EnableLogging ) Log.Info( $"Player took ${impact} damage." );
 			GetComponent<PlayerStats>().TakeDamage( collision.Self.Body.Velocity.Length );
